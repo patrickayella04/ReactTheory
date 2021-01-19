4b44069cfc19d8d1d66c8f428ce0fd23538a2420
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-01-19 15:54:31 +0000

    Link Router to new page for user login data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from '../src/components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
-import Alert from './components/layout/Alert'
-
+import './App.css';
 
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,   
     alert: null 
   }
@@ -22,6 +24,15 @@ class App extends Component {
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: res.data.items, loading: false });
+  }
+
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({loading: true})
+
+    const res = await axios.get(`https://api.github.com/users${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
   }
 
   // Clear users from state
@@ -35,7 +46,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state; // Destructuring
+    const { users, user, loading } = this.state; // Destructuring
 
     return (
       <Router> 
@@ -49,6 +60,10 @@ class App extends Component {
                   <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} setAlert={this.setAlert} />
                   <Users loading={loading} users={users}/> 
                 </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading}/>
               )} />
             </Switch>
           </div>
---
+++ src/components/users/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class User extends Component {
+    render() {
+        return (
+            <div>User</div>
+        )
+        
+    }
+}
+
+export default User
